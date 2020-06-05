7154ad5011e7fc212089c3fbcf376105a9216541
William Langlois <56624956+William-Langlois@users.noreply.github.com>
2020-06-05 10:41:33 +0200

    commit

---
+++ C'Quoi-Le-Prix.py
@@ -0,0 +1,113 @@
+#Importation des modules nécéssaires
+import requests
+import json
+import random
+from flask import Flask , render_template , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game' ,methods = ['get','post'])
+def LaunchGame():
+
+    KeywordsByCat = {
+                'Computers':['souris','ordinateur','clavier'],
+                'Vehicles' :['Jantes','Pare-Brise','Essuie-glaces'],
+                'Kitchen'  :['Casseroles','eponges']
+            }
+    
+    randomCategory = request.form['cat']
+    randomKeyword = KeywordsByCat[randomCategory][random.randint(0,len(KeywordsByCat[randomCategory])-1)]
+    print(randomCategory , randomKeyword)
+    parameters = {
+        "ApiKey": "b3b33725-547b-4368-9373-e97d94783a4d",
+        "SearchRequest": {
+        "Keyword": randomKeyword,
+        "Pagination": {
+            "ItemsPerPage": 10,
+            "PageNumber": 0
+        },
+        "Filters": {
+            "Price": {
+            "Min": 0,
+            "Max": 0
+            },
+            "Navigation": randomCategory,
+            "IncludeMarketPlace": "true"
+        }
+        }
+    }
+    Url = "https://api.cdiscount.com/OpenApi/json/Search"
+    response = requests.post(Url, data=json.dumps(parameters))
+    response = response.json()
+    randomArticle = response['Products'][random.randint(0,len(response['Products'])-1)]
+    articleName=(randomArticle['Name'])
+    articleImg=(randomArticle['MainImageUrl'])
+    articleId=(randomArticle['Id'])
+
+    try:
+        nbParties=request.form['nbParties']
+    except:
+        nbParties=0
+    
+    try:
+        absecart=round(float(request.form['absecart'])*100)/100
+    except:
+        absecart=0
+
+    return render_template('game.html',aName = articleName,aImg = articleImg,aId = articleId,nbParties=nbParties,absecart=absecart)
+
+@app.route('/try',methods = ['get','post'])
+def verifPrice():
+    url="https://api.cdiscount.com/OpenApi/json/GetProduct"
+
+    parameters={
+    "ApiKey": "b3b33725-547b-4368-9373-e97d94783a4d",
+    "ProductRequest": {
+        "ProductIdList": [
+        request.form['id']
+        ],
+        "Scope": {
+        "Offers": "false",
+        "AssociatedProducts": "false",
+        "Images": "false",
+        "Ean": "false"
+        }
+    }
+    }
+    response = requests.post(url, data=json.dumps(parameters))
+    response = response.json()
+    correctPrice =float(response['Products'][0]['BestOffer']['SalePrice'])
+    articleName= response['Products'][0]['Name']
+    articleImg = response['Products'][0]['MainImageUrl']
+    PriceEntry =float(request.form['priceEntry']) 
+    try:
+        ecart=ecart
+    except NameError:
+        ecart=0
+
+    ecart = ecart + 100-((correctPrice*100)/PriceEntry)/(float(request.form['nbParties'])+1)
+    ecart =(round(ecart*100))/100
+    absecart=abs(ecart)
+
+    ecartdirect = 100-((correctPrice*100)/PriceEntry)
+    ecartdirect =(round(ecartdirect*100))/100
+    ecartdirect = abs(ecartdirect)
+
+    return render_template('try.html',
+    aName = articleName,
+    aImg = articleImg,
+    correctPrice = correctPrice,
+    priceEntry = PriceEntry,
+    ecartdirect=ecartdirect,
+    absecart=absecart,
+    nbParties=int(request.form['nbParties'])+1
+    )
+
+
+    
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
